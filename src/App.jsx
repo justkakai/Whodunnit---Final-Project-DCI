c856b65a803bfcf51b9fe0b7504d407e558ed91e
Kakai <kenkai24@yahoo.com>
2022-09-06 16:15:31 +0200

    added buttons

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import clown from './images/scary-clown.png';
 function App() {
   return (
     <div className="App">
+      <div className="top-bar">
+        <button>Jimmy's Email Account</button>
+        <button>Recorded Conversations</button>
+        <button>Addresses already visited</button>
+        <button>CALL POLICE</button>
+      </div>
+
       <div className="game-container">
         <div className="clown-image-container">
           <img src={clown} alt="Clown" />
@@ -12,6 +19,12 @@ function App() {
           <p className='game-name'>Whodunnit?</p>
           <p className='game-descriptor'>The ultimate murder mystery game!</p>
         </div>
+      </div>
+
+      <div className="bottom-bar">
+        <button>VIEW POLICE REPORT</button>
+        <button>VIEW EVIDENCE</button>
+        <button>Search for Anything</button>
       </div>
     </div>
   );
