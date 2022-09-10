3d2a0729e85cf4d7b7d89355ead84c7116ccd63a
Kakai <kenkai24@yahoo.com>
2022-09-10 17:20:12 +0200

    added expand buttons on evidence page

--- src/components/GameRoutes/EvidencePage.jsx
+++ src/components/GameRoutes/EvidencePage.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-/* import reportItself from '../../images/reportItself.png'; */
+import { BsArrowUpRight } from 'react-icons/bs';
 import neesonInvoice from '../../images/neesonInvoice.jpg';
 import buffyLetter from '../../images/buffyLetter.jpg';
 import laurence from '../../images/laurence.jpg';
@@ -31,15 +31,39 @@ function EvidencePage() {
             <div className="evidence-page-container">
                 <div>
                     <img src={neesonInvoice} alt="Harry Neeson Invoice" />
+                    <p>
+                        <button>
+                            <span>Expand</span>
+                            <BsArrowUpRight style={{ fontSize: "1.45em", color: "white" }} />
+                        </button>
+                    </p>
                 </div>
                 <div>
                     <img src={buffyLetter} alt="Letter from Buffy" />
+                    <p>
+                        <button>
+                            <span>Expand</span>
+                            <BsArrowUpRight style={{ fontSize: "1.45em", color: "white" }} />
+                        </button>
+                    </p>
                 </div>
                 <div>
                     <img src={laurence} alt="Laurence the clown-hating religious guy" />
+                    <p>
+                        <button>
+                            <span>Expand</span>
+                            <BsArrowUpRight style={{ fontSize: "1.45em", color: "white" }} />
+                        </button>
+                    </p>
                 </div>
                 <div>
                     <img src={cocaine} alt="A bag of cocaine" />
+                    <p>
+                        <button>
+                            <span>Expand</span>
+                            <BsArrowUpRight style={{ fontSize: "1.45em", color: "white" }} />
+                        </button>
+                    </p>
                 </div>
             </div>
         </motion.section>
--- src/components/RightBarComponents/CallPoliceBtn.jsx
+++ src/components/RightBarComponents/CallPoliceBtn.jsx
@@ -1,14 +1,30 @@
+import { motion } from "framer-motion";
 import { useNavigate } from 'react-router-dom';
 import callPolice from '../../images/callPolice.svg';
 
 function CallPoliceBtn() {
 
+    const containerVariants = {
+        hidden: {
+            opacity: 0,
+        },
+        visible: {
+            opacity: 1,
+            transition: { delay: 0, duration: 0.7 }
+        }
+    }
+
     let navigate = useNavigate();
 
     return (
-        <button className="call-police-btn" onClick={() => { navigate("/call-police") }}>
+        <motion.button className="call-police-btn"
+            onClick={() => { navigate("/call-police") }}
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+        >
             <img src={callPolice} alt="Call the Police" />
-        </button>
+        </motion.button>
     )
 }
 
