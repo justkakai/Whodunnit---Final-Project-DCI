0bc16e8e8ef603561c64f23e4653968969ca191d
Kakai <kenkai24@yahoo.com>
2022-09-11 22:56:54 +0200

    added more scss variables

--- src/algo/Algo.js
+++ src/algo/Algo.js
@@ -2,7 +2,7 @@ const triggerWords = [
     { question: 'honey bunny', answer: `Hmm, I can't seem to find anything on a Honey Bunny` },
     { question: 'piotr ivanovich', answer: `Ah yes, we're currently doing a background check on Piotr Ivanovich. He has had four run-ins with law enforcement and two of those cases were drug-related.` },
     { question: 'piotr', answer: `Do you by any chance have Piotr's last name?` },
-    { question: 'jacob brandson', answer: 'Jacob Brandson was the boss of a gangster clan. He disappeared without a trace in 2007. It is likey, that he was killed, as he had many enemies / his whole gang was murdered.' },
+    { question: 'jacob brandson', answer: 'Jacob Brandson was the boss of a gangster clan. He disappeared without a trace in 2007. It is likely that he was killed, as he had many enemies.' },
     { question: 'dna', answer: `You would like to have a DNA test ran on the body of the deceased to assess that the deceased is in fact Jimmy Brandson? (yes/no)` }
 ]
 
