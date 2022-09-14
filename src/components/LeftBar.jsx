9f66d8fcdae0666d2bc824424780c16970f2375d
Kakai <kenkai24@yahoo.com>
2022-09-14 14:26:15 +0200

    search functionality added (mobile view)

--- src/components/LeftBar.jsx
+++ src/components/LeftBar.jsx
@@ -1,17 +1,15 @@
-/* import JimmyEmailBtn from './TopBarComponents/JimmyEmailBtn'; */
-/* import CallPoliceBtn from './LeftBarComponents/CallPoliceBtn'; */
 import BurgerMenu from './LeftBarComponents/BurgerMenu';
 import ConversationsBtn from './LeftBarComponents/ConversationsBtn';
+import SearchBarMobile from './LeftBarComponents/SearchBarMobile';
 import VisitedAddressesBtn from './LeftBarComponents/VisitedAddressesBtn';
 
 function LeftBar() {
 
     return (
         <section className="left-bar">
-            {/* <JimmyEmailBtn /> */}
-            {/* <CallPoliceBtn /> */}
             <ConversationsBtn />
             <VisitedAddressesBtn />
+            <SearchBarMobile />
             <BurgerMenu />
         </section>
     )
