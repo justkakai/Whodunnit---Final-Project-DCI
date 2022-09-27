ec94b897c01856bf91beef02841fe4fcd8838dd5
Reggie899 <regina.schmidt1212@gmail.com>
2022-09-27 11:09:26 +0200

    update saved

--- src/components/GameRoutes/PoliceReport.jsx
+++ src/components/GameRoutes/PoliceReport.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import { IntroPageContext } from "../../contexts/IntroPageContext";
-import reportItself from '../../images/policeReport.png';
+import reportItself from '../../images/policeReport.pgng';
 
 function PoliceReport() {
 
